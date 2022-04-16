72c669e29683a531d4432572cca907e430039c37
sjoshistrats <sjoshistrats@gmail.com>
2022-04-16 12:52:19 -0400

    bump patch

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fastgrouper",
-    version="0.1.0",
+    version="0.1.1",
     author="Shreyas Joshi",
     author_email="sjoshistrats@gmail.com",
     description="A package for applying efficient groupby operations.",
